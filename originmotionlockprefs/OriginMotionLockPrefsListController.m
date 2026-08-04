31b8a59500d5d3af7593fb78c20be643d9a0272a
duchaivcyv-design <duchaivcyv@icloud.com>
2026-08-04 20:53:05 +0700

    Create OriginMotionLockPrefsListController.m

---
+++ originmotionlockprefs/OriginMotionLockPrefsListController.m
@@ -0,0 +1,38 @@
+#import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
+
+@interface OriginMotionLockPrefsListController : PSListController
+@end
+
+@implementation OriginMotionLockPrefsListController
+
+- (NSArray *)specifiers {
+    if (!_specifiers) {
+        _specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
+    }
+    return _specifiers;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIBarButtonItem *respringButton = [[UIBarButtonItem alloc] initWithTitle:@"Lưu & Respring" 
+                                                                       style:UIBarButtonItemStylePlain 
+                                                                      target:self 
+                                                                      action:@selector(respringAction)];
+    self.navigationItem.rightBarButtonItem = respringButton;
+}
+
+- (void)respringAction {
+    CFNotificationCenterPostNotification(
+        CFNotificationCenterGetDarwinNotifyCenter(),
+        CFSTR("com.yourname.originmotionlock/prefsupdated"),
+        NULL,
+        NULL,
+        YES
+    );
+    pid_t pid;
+    const char *args[] = {"sbreload", NULL, NULL};
+    posix_spawn(&pid, "/usr/bin/sbreload", NULL, NULL, (char *const *)args, NULL);
+}
+
+@end
