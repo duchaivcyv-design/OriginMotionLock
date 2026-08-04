ce4f0818061fbb41000fdb26c4682601cdbe59ad
duchaivcyv-design <duchaivcyv@icloud.com>
2026-08-04 12:01:20 +0700

    Update OriginMotionLockPrefsListController.m

--- OriginMotionLockPrefsListController.m
+++ OriginMotionLockPrefsListController.m
@@ -8,7 +8,6 @@
 
 - (NSArray *)specifiers {
     if (!_specifiers) {
-        // Tự động load file Root.plist để hiển thị giao diện trong Cài đặt
         _specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
     }
     return _specifiers;
