8667c5f57375056699c301825bb3c6b8baa6dad2
duchaivcyv-design <duchaivcyv@icloud.com>
2026-08-04 10:42:27 +0700

    Update OMLRootListController.m

--- preference/OMLRootListController.m
+++ preference/OMLRootListController.m
@@ -1,19 +1,43 @@
 #import <UIKit/UIKit.h>
 
-@interface PSListController : UIViewController {
+@interface OMLRootListController : UIViewController {
     NSArray *_specifiers;
 }
-- (NSArray *)loadSpecifiersFromPlistName:(NSString *)name target:(id)target;
-@end
-
-@interface OMLRootListController : PSListController
 @end
 
 @implementation OMLRootListController
-- (NSArray *)specifiers {
+
+- (id)specifiers {
     if (!_specifiers) {
-        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+        // Tự tạo danh sách giao diện cài đặt cơ bản trực tiếp không lệ thuộc plist hệ thống
+        NSMutableArray *specifiers = [NSMutableArray array];
+        
+        // Nhóm cài đặt chính
+        id groupSpecifier = [NSClassFromString(@"PSSpecifier") preferenceSpecifierNamed:@"OriginMotionLock Cài đặt" target:nil set:nil get:nil detail:nil cell:PSGroupCell edit:nil];
+        [specifiers addObject:groupSpecifier];
+        
+        // Nút bật/tắt hiệu ứng
+        id switchSpecifier = [NSClassFromString(@"PSSpecifier") preferenceSpecifierNamed:@"Bật hiệu ứng" target:self set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:) detail:nil cell:PSSwitchCell edit:nil];
+        [switchSpecifier setProperty:@"isEnabled" forKey:@"key"];
+        [switchSpecifier setProperty:@YES forKey:@"default"];
+        [switchSpecifier setProperty:@"com.yourname.originmotionlock" forKey:@"defaults"];
+        [specifiers addObject:switchSpecifier];
+        
+        _specifiers = [specifiers copy];
     }
     return _specifiers;
 }
+
+- (id)readPreferenceValue:(id)specifier {
+    NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:@"/var/jb/var/mobile/Library/Preferences/com.yourname.originmotionlock.plist"];
+    return [prefs objectForKey:[specifier propertyForKey:@"key"]] ?: @YES;
+}
+
+- (void)setPreferenceValue:(id)value specifier:(id)specifier {
+    NSMutableDictionary *prefs = [NSMutableDictionary dictionaryWithContentsOfFile:@"/var/jb/var/mobile/Library/Preferences/com.yourname.originmotionlock.plist"] ?: [NSMutableDictionary dictionary];
+    [prefs setObject:value forKey:[specifier propertyForKey:@"key"]];
+    [prefs writeToFile:@"/var/jb/var/mobile/Library/Preferences/com.yourname.originmotionlock.plist" atomically:YES];
+    CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), CFSTR("com.yourname.originmotionlock/settingschanged"), NULL, NULL, YES);
+}
+
 @end
