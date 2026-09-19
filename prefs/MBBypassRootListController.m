d38ef71c398eab32a66f31abfef2faf4e7158661
duchaivcyv-design <duchaivcyv@icloud.com>
2026-09-20 03:05:32 +0700

    Update MBBypassRootListController.m

--- prefs/MBBypassRootListController.m
+++ prefs/MBBypassRootListController.m
@@ -14,19 +14,9 @@
 
 - (NSArray *)specifiers {
     if (!_specifiers) {
-        NSMutableArray *specifiers = [NSMutableArray array];
+        // Tải các thành phần tĩnh từ Root.plist trước
+        _specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] mutableCopy];
 
-        // 1. Nhóm cấu hình chung
-        PSSpecifier *groupGeneral = [PSSpecifier preferenceSpecifierNamed:@"Cài đặt chung" target:self set:nil get:nil detail:Nil cell:PSGroupCell edit:Nil];
-        [specifiers addObject:groupGeneral];
-
-        PSSpecifier *switchGlobal = [PSSpecifier preferenceSpecifierNamed:@"Bật/Tắt Toàn Hệ Thống" target:self set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:) detail:Nil cell:PSSwitchCell edit:Nil];
-        [switchGlobal setProperty:@"com.onyx.mbbypass" forKey:@"defaults"];
-        [switchGlobal setProperty:@"isEnabled" forKey:@"key"];
-        [switchGlobal setProperty:@YES forKey:@"default"];
-        [specifiers addObject:switchGlobal];
-
-        // Khởi tạo các mảng chứa ứng dụng theo từng nhóm phân loại
         NSMutableArray *bankApps = [NSMutableArray array];
         NSMutableArray *socialApps = [NSMutableArray array];
         NSMutableArray *otherApps = [NSMutableArray array];
@@ -37,18 +27,15 @@
                 id workspace = [LSWorkspace performSelector:@selector(defaultWorkspace)];
                 NSArray *installedApps = [workspace performSelector:@selector(allInstalledApplications)];
                 
-                // Sắp xếp theo tên chữ cái A-Z
+                // Sắp xếp danh sách ứng dụng theo tên A-Z
                 NSArray *sortedApps = [installedApps sortedArrayUsingComparator:^NSComparisonResult(id app1, id app2) {
                     NSString *name1 = [app1 performSelector:@selector(localizedName)];
                     NSString *name2 = [app2 performSelector:@selector(localizedName)];
                     return [name1 localizedCompare:name2];
                 }];
 
-                // Danh sách từ khóa nhận diện nhanh app Ngân hàng / Tài chính phổ biến
-                NSArray *bankKeywords = @[@"bank", @"pay", @"wallet", @"ví", @"momo", @"zalopay", @"vnpay", @"shopeepay", @"timo", @"VCB", @"techcombank", @"MBBank", @"acb", @"bidv", @"vietinbank", @"agribank", @"sacombank", @"vpbank", @"tpbank", @"msb", @"seabank", @"eximbank", @"vib", @"ocb", @"hsbc", @"shinhan", @"cake", @"kbank", @"cimb"];
-                
-                // Danh sách từ khóa nhận diện mạng xã hội / giải trí
-                NSArray *socialKeywords = @[@"facebook", @"messenger", @"instagram", @"tiktok", @"zalo", @"telegram", @"whatsapp", @"twitter", @"reddit", @"discord", @"netflix", @"youtube", @"spotify"];
+                NSArray *bankKeywords = @[@"bank", @"pay", @"wallet", @"ví", @"momo", @"zalopay", @"vnpay", @"shopeepay", @"timo", @"vcb", @"techcombank", @"mbbank", @"acb", @"bidv", @"vietinbank", @"agribank", @"sacombank", @"vpbank", @"tpbank", @"msb", @"seabank", @"eximbank", @"vib", @"ocb", @"hsbc", @"shinhan", @"cake", @"kbank", @"cimb"];
+                NSArray *socialKeywords = @[@"facebook", @"messenger", @"instagram", @"tiktok", @"zalo", @"telegram", @"whatsapp", @"twitter", @"reddit", @"discord"];
 
                 for (id app in sortedApps) {
                     NSString *bundleID = [app performSelector:@selector(applicationIdentifier)];
@@ -62,9 +49,8 @@
                         PSSpecifier *appSwitch = [PSSpecifier preferenceSpecifierNamed:appName target:self set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:) detail:Nil cell:PSSwitchCell edit:Nil];
                         [appSwitch setProperty:@"com.onyx.mbbypass" forKey:@"defaults"];
                         [appSwitch setProperty:key forKey:@"key"];
-                        [appSwitch setProperty:@NO forKey:@"default"]; // Mặc định tắt
+                        [appSwitch setProperty:@NO forKey:@"default"]; // Mặc định là TẮT
 
-                        // Phân loại vào danh mục
                         NSString *lowerBundle = [bundleID lowercaseString];
                         NSString *lowerName = [appName lowercaseString];
                         
@@ -95,32 +81,28 @@
                 }
             }
         } @catch (NSException *exception) {
-            NSLog(@"[MBBypass] Error categorizing apps: %@", exception);
+            NSLog(@"[MBBypass] Error: %@", exception);
         }
 
-        // 2. Thêm nhóm Ứng dụng Tài chính / Ngân hàng
+        // Bổ sung các nhóm app có sẵn chữ tiêu đề và nút bật tắt tự động
         if ([bankApps count] > 0) {
             PSSpecifier *groupBank = [PSSpecifier preferenceSpecifierNamed:@"Ứng dụng Ngân hàng & Tài chính" target:self set:nil get:nil detail:Nil cell:PSGroupCell edit:Nil];
-            [groupBank setProperty:@"Các ứng dụng thanh toán, ngân hàng cần bật chế độ ẩn:" forKey:@"footerText"];
-            [specifiers addObject:groupBank];
-            [specifiers addObjectsFromArray:bankApps];
+            [groupBank setProperty:@"Bật công tắc riêng cho các app ngân hàng cần ẩn:" forKey:@"footerText"];
+            [_specifiers addObject:groupBank];
+            [_specifiers addObjectsFromArray:bankApps];
         }
 
-        // 3. Thêm nhóm Mạng xã hội & Giải trí
         if ([socialApps count] > 0) {
-            PSSpecifier *groupSocial = [PSSpecifier preferenceSpecifierNamed:@"Mạng xã hội & Giải trí" target:self set:nil get:nil detail:Nil cell:PSGroupCell edit:Nil];
-            [specifiers addObject:groupSocial];
-            [specifiers addObjectsFromArray:socialApps];
+            PSSpecifier *groupSocial = [PSSpecifier preferenceSpecifierNamed:@"Mạng xã hội & Trò chuyện" target:self set:nil get:nil detail:Nil cell:PSGroupCell edit:Nil];
+            [_specifiers addObject:groupSocial];
+            [_specifiers addObjectsFromArray:socialApps];
         }
 
-        // 4. Thêm nhóm Các ứng dụng khác
         if ([otherApps count] > 0) {
-            PSSpecifier *groupOther = [PSSpecifier preferenceSpecifierNamed:@"Các ứng dụng khác" target:self set:nil get:nil detail:Nil cell:PSGroupCell edit:Nil];
-            [specifiers addObject:groupOther];
-            [specifiers addObjectsFromArray:otherApps];
+            PSSpecifier *groupOther = [PSSpecifier preferenceSpecifierNamed:@"Các ứng dụng khác trong máy" target:self set:nil get:nil detail:Nil cell:PSGroupCell edit:Nil];
+            [_specifiers addObject:groupOther];
+            [_specifiers addObjectsFromArray:otherApps];
         }
-
-        _specifiers = [specifiers copy];
     }
     return _specifiers;
 }
